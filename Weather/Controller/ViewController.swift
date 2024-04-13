d7b60147aeb98caac049acc9ed4616f49aa374d8
milena <milimaalc.dev@gmail.com>
2024-04-13 16:39:39 -0300

    created the headerView

---
+++ Weather/Controller/ViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Milena Alcântara on 13/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Lazy é uma variável 'preguiçosa', ela só será criada quando for feita a primeira referência a ela
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.text = "São Paulo"
+        label.textAlignment = .center
+        label.textColor = UIColor(named: "ColorBlue")
+        return label
+    }()
+
+    private lazy var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 70, weight: .bold)
+        label.text = "25ºC"
+        label.textAlignment = .center
+        label.textColor = UIColor(named: "ColorBlue")
+        return label
+    }()
+    
+    private lazy var weatherIcon: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "daylight")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // executa sempre que essa vc é carregada (apenas 1 vez)
+        // Do any additional setup after loading the view.
+        
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // executa sempre que a tela aparecer Ex: mudei de tela e voltei pra essa
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(backgroundView)
+        view.addSubview(headerView)
+        
+        headerView.addSubview(cityLabel)
+        headerView.addSubview(temperatureLabel)
+        headerView.addSubview(weatherIcon)
+    }
+    
+    private func setConstraints() {
+        setBackgroundViewConstraints()
+        setHeaderViewConstraints()
+        setCityLabelConstraints()
+        setTemperatureLabelConstraints()
+        setWeatherIconConstraints()
+    }
+    
+    private func setBackgroundViewConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setHeaderViewConstraints() {
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            headerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 35),
+            headerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -35),
+            headerView.heightAnchor.constraint(equalToConstant: 169)
+        ])
+    }
+    
+    private func setCityLabelConstraints() {
+        NSLayoutConstraint.activate([
+            cityLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+            cityLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
+            cityLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15),
+            cityLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    private func setTemperatureLabelConstraints() {
+        NSLayoutConstraint.activate([
+            temperatureLabel.topAnchor.constraint(equalTo: cityLabel.topAnchor, constant: 41),
+            temperatureLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 26),
+        ])
+    }
+    
+    private func setWeatherIconConstraints() {
+        NSLayoutConstraint.activate([
+            weatherIcon.heightAnchor.constraint(equalToConstant: 86),
+            weatherIcon.widthAnchor.constraint(equalToConstant: 86),
+            weatherIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -26),
+            weatherIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor),
+            weatherIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor, constant: 14),
+            
+        ])
+    }
+    
+}
+
